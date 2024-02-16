68aef2538df90d1fb1ba297a3ba86b9061afa3cf
BenBean18 <beakerthespeaker@gmail.com>
2024-02-16 10:57:02 -0500

    configuration changes

--- record_video.sh
+++ record_video.sh
@@ -2,4 +2,4 @@
 # Note: stream.ts should be a named pipe, run `mkfifo stream.ts` to create it
 cd /home/rocket/RocketVideo
 sudo nohup ./rpidatv/bin/rpidatv -i stream.ts -s 1000 -c 1/2 -m RF -f 423 -p 7 -l > /dev/null 2>&1 &
-sudo nohup python3 video_recorder.py > /dev/null 2>&1 &
+nohup python3 video_recorder.py > /dev/null 2>&1 &
--- video_recorder.py
+++ video_recorder.py
@@ -5,19 +5,19 @@ from picamera2.outputs import FileOutput, FfmpegOutput
 from libcamera import controls
 import time
 picam2 = Picamera2()
-config = picam2.create_video_configuration(main={"size": (1600, 900)}, lores={"size": (480, 360)}, encode="main")
+config = picam2.create_video_configuration(main={"size": (1536, 864)}, lores={"size": (640, 360)}, encode="main")
 picam2.configure(config)
 
 picam2.set_controls({"FrameRate": 20, "AfMode": controls.AfModeEnum.Continuous})
 
 h264_encoder = H264Encoder()
-h264_output = "recording.h264"
+h264_output = f"recording_{time.strftime('%Y-%m-%d_%H-%M-%S')}.h264"
 
 lores_encoder = H264Encoder()
 lores_encoder.size = config["lores"]["size"]
 lores_encoder.format = config["lores"]["format"]
 lores_encoder.bitrate = 800000
-lores_encoder.output = FfmpegOutput("-f mpegts -minrate 700k -maxrate 900k -bufsize 1000 -muxrate 921568 stream.ts")
+lores_encoder.output = FfmpegOutput("-f mpegts -minrate 700k -maxrate 900k -bufsize 10000 -muxrate 921568 stream.ts")
 lores_encoder.repeat = True
 lores_encoder.iperiod = 10
 lores_encoder.start()
